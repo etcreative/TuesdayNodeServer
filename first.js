3ae973716dd29c0af47db34aef88334eddd88ab0
wsadmin <wsadmin>
2017-02-28 19:38:56 -0500

    new

--- first.js
+++ first.js
@@ -1,28 +1,29 @@
 var http = require("http");
-var PORT = 7000;
+var PORT1 = 7000;
+var PORT2 = 7500;
 
-function handleRequest(request, response) {
+function handleRequest1(request, response) {
 	response.end("You are sexy! Path hit: " + request.url);
 }
 
-var server = http.createServer(handleRequest);
+var server1 = http.createServer(handleRequest1);
 
-server.listen(PORT, function() {
-	console.log("Server listening on: http://localhost:%s", PORT);
+server1.listen(PORT1, function() {
+	console.log("Server listening on: http://localhost:%s", PORT1);
 })
 
 
-var http = require("http");
-var PORT = 7500;
 
-function handleRequest(request, response) {
+
+
+function handleRequest2(request, response) {
 	response.end("You are ugly!: " + request.url);
 }
 
-var server = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest2);
 
-server.listen(PORT, function() {
-	console.log("Server listening on: http://localhost:%s", PORT);
+server2.listen(PORT2, function() {
+	console.log("Server listening on: http://localhost:%s", PORT2);
 })
 
 
